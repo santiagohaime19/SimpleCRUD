653b2999451c114416258892008bd3c55386720c
james.mendoza <james.mendoza@whitecloak.com>
2021-07-12 10:47:30 +0800

    First Commit

---
+++ simpleCRUD/src/main/kotlin/com/james/simpleCRUD/simpleCRUD/SimpleCrudApplication.kt
@@ -0,0 +1,11 @@
+package com.james.simpleCRUD.simpleCRUD
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SimpleCrudApplication
+
+fun main(args: Array<String>) {
+	runApplication<SimpleCrudApplication>(*args)
+}
---
+++ simpleCRUD/src/test/kotlin/com/james/simpleCRUD/simpleCRUD/SimpleCrudApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.james.simpleCRUD.simpleCRUD
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SimpleCrudApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
